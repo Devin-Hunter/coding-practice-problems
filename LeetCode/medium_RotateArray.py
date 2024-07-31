1ddd2e6486125d7512374758c45fba057f215fd8
Devin-Matherne <devinmatherne@ymail.com>
2024-07-31 10:11:38 -0600

    Added new LeetCode Challenge: RotateArray

---
+++ LeetCode/medium_RotateArray.py
@@ -0,0 +1,38 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+"""
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        """
+        This method doesn't work for all cases and I'm not sure why. For some test cases it works with k, but for others it works with k + 1.
+        In Thonny, k works for both test cases when entered manually without any changes.
+        """
+        r = nums[0:k]
+        del nums[0:k]
+
+        return nums.extend(r)
