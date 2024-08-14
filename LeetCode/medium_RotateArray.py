50c30c9f15c5368c5965088ec72169340090472d
Devin-Matherne <devinmatherne@ymail.com>
2024-08-13 18:01:08 -0600

    Added another failed solution to RotateArray

--- LeetCode/medium_RotateArray.py
+++ LeetCode/medium_RotateArray.py
@@ -29,13 +29,38 @@ class Solution(object):
         """
 
         """
+        This method works to rotate the list
+        """
+
+        """
+        My first method wasn't working correctly because it didn't account for if k was even or odd, which will change how many elements need to be moved
+
+        This method does account for even or odd k value, but it fails if k == 1 for a list that only has 2 items and if k == 3 for the same 2 item list
+
+        It fails because in the k = 3 test case because the list is only 2 items long, but k = 3, meaning there aren't enough elements to delete and
+        move to the front. My lazy method won't work for these cases. I will need to actually get the list to shift k amount of times, removing the last element
+        and inserting it into the front for these test cases to pass.
+        """
+
+        if k % 2 == 0:
+            r = nums[0:k]
+            del nums[0:k]
+        else:
+            r = nums[0:k + 1]
+            del nums[0:k + 1]
+
+        nums.extend(r)
+
+        return nums
+
+        """
         This method doesn't work for all cases and I'm not sure why. For some test cases it works with k, but for others it works with k + 1.
         In Thonny, k works for both test cases when entered manually without any changes.
 
         If I move k amount of elements from the front of the list to the end, I thought it would be the same for every list. However, for some lists,
         moving k amount of elements isn't enough elements.
         """
-        r = nums[0:k]
-        del nums[0:k]
+        # r = nums[0:k]
+        # del nums[0:k]
 
-        return nums.extend(r)
+        # return nums.extend(r)
