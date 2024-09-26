ba4769ba2bf9061438adb5b707993ac9a42bef69
Devin-Matherne <devinmatherne@ymail.com>
2024-09-25 23:30:58 -0600

    Added new STATION Table to SQL Folder

---
+++ HackerRank/SQL/STATION Table/STATION.py
@@ -0,0 +1,9 @@
+"""
+        STATION
+Field       |       Type
+ID          |       NUMBER
+CITY        |       VARCHAR2(21)
+STATE       |       VARCHAR2(2)
+LAT_N       |       NUMBER
+LONG_W      |       NUMBER
+"""
